c7027c13dbd09e8eba46ca5e5fe512963208a557
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2023-12-26 20:24:11 +0200

    Update Chain.ts

--- src/Chain.ts
+++ src/Chain.ts
@@ -80,15 +80,16 @@ export class Chain
      { 
          let b=Buffer.concat([buff,Buffer.from(Chain.address.toString())])
          _address=b.toString("hex")
-         console.log(_address)
+         //console.log(_address)
          address=new Address(_address)
          address.setTransfer(this)
          this.addresses.push(address.address)
          Chain.address+=1
+         console.log(address)
         
       }
      })
-    console.log(address)
+    
     return address 
      
  }
