ec9564a74c8e566f13ac7e2bc14f9ab215e31101
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2023-12-26 12:33:28 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,20 @@ import "reflect-metadata";
 import application from "express"
 import { json,urlencoded } from "express";
 import cors from "cors";
+import {Chain} from "./Chain"
 //import { AppDataSource } from "./_datasource";
 //import { apiv1 } from "./routes";
 const app=application();
 const port = process.env.PORT||3000
-
+const chain= new Chain()
 app.use(urlencoded({extended: true}))
 app.use(cors())
 app.use(json())
 //app.use(apiv1)
 
+app.get("/",(req,res)=>{
+  res.json(chain)
+})
 app.use((error:any, req:any, res:any, next:any) => {
   console.log(error)
   console.log("Error Handling Middleware called")
