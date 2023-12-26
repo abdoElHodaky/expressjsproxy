61da8735a433ef09bd5c50d1e496eb88701e48a5
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2023-12-26 12:19:16 +0200

    Create User.ts

---
+++ User.ts
@@ -0,0 +1,58 @@
+export class Address {
+    public transferHistory=[]
+    user:string=""
+    address:string=""
+    balance:number=1000
+    private transfer:Transfer
+    constructor(address,user=""){
+        this.address=address
+        this.user=user
+    }
+    setTransfer(chain){
+        this.transfer=new Transfer(chain)
+    }
+    transferTo(to, amount){
+        this.transfer.from=this
+        this.transfer.to=to
+        //this.transfer.amount=amount
+        this.transfer.transfer(amount)
+        this.transferHistory.push({
+            from:this.address,
+            to:to.address,
+            timestamp:this.transfer.gettimestamp(),
+            amount:amount
+        })
+    }
+    
+}
+
+export class Transfer {
+ public from:Address=<Address>{}
+ public to:Address=<Address>{}
+ private timestamp=0
+  private chain:Chain
+  constructor(chain){
+      this.chain=chain
+  }
+  transfer(amount){
+      if ((chain.checkAddress(this.from.address))
+          &&(chain.checkAddress(this.to.address)))
+           {
+            chain.addtrans(chain.getlast(),
+      this.from.address,
+      this.to.address,
+      amount               
+       )
+      amount-=chain.getfee()
+      this.timestamp=Date.now()
+      this.from.balance-=amount
+      this.to.balance+=amount
+      }
+      else{
+          console.log("check addresses")
+      }
+  }
+   gettimestamp (){return this.timestamp;}
+
+    
+}
