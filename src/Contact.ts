a7a95d17d98beb8f17b60faf6f4050bba645413d
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2023-12-27 18:03:03 +0200

    Create Contact.ts

---
+++ src/Contact.ts
@@ -0,0 +1,10 @@
+export class Contract{
+ private supply:number 
+ private symbol:string
+ private logo:any
+ private permissions:[]
+ private burnable:boolean
+ private tradeble:boolean
+ approve(){}
+ revoke(){}
+}
