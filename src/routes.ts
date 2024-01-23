6974fc1f02d1e57a42c41c4726fd384a434ac266
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-01-23 12:19:24 +0200

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,22 @@
 import Router from "express"
 
 let routes=Router()
+
+routes.get("/genAddress",(req,res)=>{
+  let address=chain.createAddress()
+  console.log(address)
+  res.json({
+    address: address.address
+  })
+})
+routes.post("/:sender/createTrans",(req,res)=>{
+  let sender=req.params.sender
+  let receiver=req.body.receiver
+  let amount=req.body.amount
+  chain.syncT()
+  res.status(200).end("done")
+})
+app.get("/explorer/AllTrans",(req,res)=>{
+  res.json(explorer.getTrans())
+})
+export routes
