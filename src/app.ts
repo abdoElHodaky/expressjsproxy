5f895db94b213e87c438b20742bbf743b9f23c6e
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-01-23 14:29:44 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -49,14 +49,16 @@ app.post("/:sender/createTrans",(req,res)=>{
 app.get("/explorer/AllTrans",(req,res)=>{
   res.json(explorer.getTrans())
 })*/
-app.use("/",routes)
+app.use("/api",routes)
 app.use("*",(error:any, req:any, res:any, next:any) => {
   console.log(error)
   console.log("Error Handling Middleware called")
   console.log('Path: ', req.path)
   next() // (optional) invoking next middleware
 })
-
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
