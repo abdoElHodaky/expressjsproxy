d3b83d3cdc193f9bd1f1db88504d38fba2c3fbd1
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-02-28 11:38:05 +0200

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,47 +1,10 @@
 import Router from "express"
-import {chain,explorer} from "./app"
-import {Address} from "./User"
+import {Client} from "./User"
 export const routes=Router()
-let request=require("request")
-routes.get("/genAddress",(req,res)=>{
-  let address=chain.createAddress()
-  console.log(address)
-  res.json({
-    address: address
-  })
-})
-
-routes.post("/createTrans",(req,res)=>{
-  let sender=req.body.sender
-  let receiver=req.body.receiver
-  let amount:number=req.body.amount
-  //let addsender=new Address(sender)
- // addsender.
-  sender.setTransfer(chain)
-  receiver.setTransfer(chain)
-  sender.transferTo(receiver,amount)
-  request.post({
-    url:"/confirm",
-    function (error, response, body){
-      res.send(body)
-    }
-  })
-  res.status(200).json(
-    [...sender.transferHistory.reverse()]
-  )
-})
-
-routes.get("/explorer/AllTrans",(req,res)=>{
-  res.json(explorer.getTrans())
-})
-
-routes.post("/confirm",(req,res)=>{
-  if(chain.pending_trans.length>=chain.maxTrans)
-  { chain.confirm()
-    chain.syncT()
-    res.end("confirmed")
-  }
-  else
-    res.end("")
-  
+//let request=require("request")
+let client = new Client()
+routes.get("intercept/",(req,res)=>{
+  let options=req.body.options
+  client.processRequest(options)
+  res.send(client.result)
 })
