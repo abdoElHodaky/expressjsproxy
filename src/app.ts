cbc69315ddde0863ade2c272a2a6986a607943e6
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-02-28 11:02:43 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,15 +3,11 @@ import "reflect-metadata";
 import application from "express"
 import { json,urlencoded } from "express";
 import cors from "cors";
-import {Chain} from "./Chain"
-import {Explorer} from "./Explorer"
+
 import {routes} from "./routes"
-//import { AppDataSource } from "./_datasource";
-//import { apiv1 } from "./routes";
 const app=application();
 const port = process.env.PORT||3000
-export const chain= new Chain()
-export const explorer=new Explorer(chain)
+
 app.use(urlencoded({extended: true}))
 app.use(cors())
 app.use(json())
@@ -22,19 +18,16 @@ const express=require("express")
 const fs=require("fs")
 app.use(express.static("/"))
 app.get("/",(req,res)=>{
-  res.redirect("/apidocs")
+  //res.redirect("/apidocs")
 })
 
-app.use("/api",routes)
+//app.use("/api",routes)
 app.use("*",(error:any, req:any, res:any, next:any) => {
   console.log(error)
   console.log("Error Handling Middleware called")
   console.log('Path: ', req.path)
   next() // (optional) invoking next middleware
 })
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../swagger.json');
-app.use('/apidocs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
